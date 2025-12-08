579121f5c25346c646f971fb6febb4f587eb07eb
NhatPhat307 <nhatphat30706@gmail.com>
2025-12-09 05:33:18 +0700

    Add .gitignore and refactor the old shape's struct

--- main.cpp
+++ main.cpp
@@ -1,69 +1,161 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+#include <vector>
+#include <ctime>
+
 using namespace std;
+
 #define H 20
 #define W 15
-char board[H][W] = {} ;
-char blocks[][4][4] = {
-        {{' ','I',' ',' '},
-         {' ','I',' ',' '},
-         {' ','I',' ',' '},
-         {' ','I',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','T',' ',' '},
-         {'T','T','T',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','S','S',' '},
-         {'S','S',' ',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {'Z','Z',' ',' '},
-         {' ','Z','Z',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {'J',' ',' ',' '},
-         {'J','J','J',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ',' ','L',' '},
-         {'L','L','L',' '},
-         {' ',' ',' ',' '}}
+
+char board[H][W] = {};
+int x = 4, y = 0;
+int speed = 200;
+
+class Piece {
+public:
+    char shape[4][4];
+
+    Piece() {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                shape[i][j] = ' ';
+    }
+
+    virtual ~Piece() {}
+
+    virtual void rotate(int currentX, int currentY) {
+        char temp[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                temp[j][3 - i] = shape[i][j];
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (temp[i][j] != ' ') {
+                    int tx = currentX + j;
+                    int ty = currentY + i;
+                    if (tx < 1 || tx >= W - 1 || ty >= H - 1) return; 
+                    if (board[ty][tx] != ' ') return; 
+                }
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                shape[i][j] = temp[i][j];
+            }
+        }
+    }
 };
 
-int x=4,y=0,b=1;
-int speed = 200;    
+class IPiece : public Piece {
+public:
+    IPiece() {
+        shape[0][1] = 'I'; shape[1][1] = 'I'; shape[2][1] = 'I'; shape[3][1] = 'I';
+    }
+};
+
+class OPiece : public Piece {
+public:
+    OPiece() {
+        shape[1][1] = 'O'; shape[1][2] = 'O';
+        shape[2][1] = 'O'; shape[2][2] = 'O';
+    }
+    void rotate(int currentX, int currentY) override {
+    }
+};
+
+class TPiece : public Piece {
+public:
+    TPiece() {
+        shape[1][1] = 'T';
+        shape[2][0] = 'T'; shape[2][1] = 'T'; shape[2][2] = 'T';
+    }
+};
+
+class SPiece : public Piece {
+public:
+    SPiece() {
+        shape[1][1] = 'S'; shape[1][2] = 'S';
+        shape[2][0] = 'S'; shape[2][1] = 'S';
+    }
+};
+
+class ZPiece : public Piece {
+public:
+    ZPiece() {
+        shape[1][0] = 'Z'; shape[1][1] = 'Z';
+        shape[2][1] = 'Z'; shape[2][2] = 'Z';
+    }
+};
+
+class JPiece : public Piece {
+public:
+    JPiece() {
+        shape[1][0] = 'J';
+        shape[2][0] = 'J'; shape[2][1] = 'J'; shape[2][2] = 'J';
+    }
+};
+
+class LPiece : public Piece {
+public:
+    LPiece() {
+        shape[1][2] = 'L';
+        shape[2][0] = 'L'; shape[2][1] = 'L'; shape[2][2] = 'L';
+    }
+};
+
+Piece* currentPiece = nullptr;
+
+Piece* createRandomPiece() {
+    int r = rand() % 7;
+    switch (r) {
+        case 0: return new IPiece();
+        case 1: return new OPiece();
+        case 2: return new TPiece();
+        case 3: return new SPiece();
+        case 4: return new ZPiece();
+        case 5: return new JPiece();
+        case 6: return new LPiece();
+        default: return new IPiece();
+    }
+}
+
 void gotoxy(int x, int y) {
-    COORD c = {x, y};
+    COORD c = { (SHORT)x, (SHORT)y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
-void boardDelBlock(){
-    for (int i = 0 ; i < 4 ; i++)
-        for (int j = 0 ; j < 4 ; j++)
-            if (blocks[b][i][j] != ' ' && y+j < H)
-                board[y+i][x+j] = ' ';
+
+void boardDelBlock() {
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (currentPiece->shape[i][j] != ' ' && y + j < H)
+                board[y + i][x + j] = ' ';
 }
-void block2Board(){
-    for (int i = 0 ; i < 4 ; i++)
-        for (int j = 0 ; j < 4 ; j++)
-            if (blocks[b][i][j] != ' ' )
-                board[y+i][x+j] = blocks[b][i][j];
+
+void block2Board() {
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (currentPiece->shape[i][j] != ' ')
+                board[y + i][x + j] = currentPiece->shape[i][j];
 }
-void initBoard(){
-    for (int i = 0 ; i < H ; i++)
-        for (int j = 0 ; j < W ; j++)
-            if ((i==H-1) || (j==0) || (j == W-1)) board[i][j] = '#';
+
+void initBoard() {
+    for (int i = 0; i < H; i++)
+        for (int j = 0; j < W; j++)
+            if ((i == H - 1) || (j == 0) || (j == W - 1)) board[i][j] = '#';
             else board[i][j] = ' ';
 }
+
 void setColor(int color) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, color);
 }
+
 void draw() {
     gotoxy(0, 0);
     for (int i = 0; i < H; i++, cout << endl) {
@@ -73,89 +165,62 @@ void draw() {
             }
             else if (board[i][j] == '#') {
                 setColor(8);
-                cout << "\xB2\xB2"; 
+                cout << "\xB2\xB2";
                 setColor(7);
             }
             else {
                 switch (board[i][j]) {
-                    case 'I': setColor(11); break; // Cyan
-                    case 'J': setColor(9);  break; // Blue
-                    case 'L': setColor(6);  break; // Orange brown
-                    case 'O': setColor(14); break; // Yellow
-                    case 'S': setColor(10); break; // Green
-                    case 'T': setColor(13); break; // Purple
-                    case 'Z': setColor(12); break; // Red
-                    default:  setColor(7);  break; // White
+                case 'I': setColor(11); break;
+                case 'J': setColor(9);  break;
+                case 'L': setColor(6);  break;
+                case 'O': setColor(14); break;
+                case 'S': setColor(10); break;
+                case 'T': setColor(13); break;
+                case 'Z': setColor(12); break;
+                default:  setColor(7);  break;
                 }
                 cout << "[]";
-
-                setColor(7); 
+                setColor(7);
             }
         }
     }
 }
-bool canMove(int dx, int dy){
-    for (int i = 0 ; i < 4 ; i++)
-        for (int j = 0 ; j < 4 ; j++)
-            if (blocks[b][i][j] != ' '){
+
+bool canMove(int dx, int dy) {
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (currentPiece->shape[i][j] != ' ') {
                 int tx = x + j + dx;
                 int ty = y + i + dy;
-                if ( tx<1 || tx >= W-1 || ty >= H-1) return false;
-                if ( board[ty][tx] != ' ') return false;
+                if (tx < 1 || tx >= W - 1 || ty >= H - 1) return false;
+                if (board[ty][tx] != ' ') return false;
             }
     return true;
 }
 
-void removeLine(){
-    for(int i = H - 2; i > 0; i--){
+void removeLine() {
+    for (int i = H - 2; i > 0; i--) {
         bool isFull = true;
-        for(int j = 1; j < W-1; j++){
-            if(board[i][j] == ' '){
+        for (int j = 1; j < W - 1; j++) {
+            if (board[i][j] == ' ') {
                 isFull = false;
             }
         }
-        if(isFull){
-            for(int k = i ; k > 0 ; k--){
-                for(int j = 1; j < W-1; j++){
-                    // if the line is the first one => it only remove that line
-                    if(k != 1){
-                        board[k][j] = board[k-1][j];
+        if (isFull) {
+            for (int k = i; k > 0; k--) {
+                for (int j = 1; j < W - 1; j++) {
+                    if (k != 1) {
+                        board[k][j] = board[k - 1][j];
                     }
-                    else{
+                    else {
                         board[k][j] = ' ';
                     }
                 }
             }
             i++;
-             //recheck: whether the new line is full
-            if (speed < 400){
-                speed += 50;
+            if (speed < 400) {
+                speed += 50; 
             }
-        }
-       
-    }
-}
-
-void rotateBlock() {
-    char temp[4][4];
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            temp[j][3 - i] = blocks[b][i][j];
-        }
-    }
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            if (temp[i][j] != ' ') {
-                int tx = x + j;
-                int ty = y + i;
-                if (tx < 1 || tx >= W - 1 || ty >= H - 1) return; 
-                if (board[ty][tx] != ' ') return; 
-            }
-        }
-    }
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            blocks[b][i][j] = temp[i][j];
         }
     }
 }
@@ -163,24 +228,28 @@ void rotateBlock() {
 int main()
 {
     srand(time(0));
-    b = rand() % 7;
+    
+    currentPiece = createRandomPiece();
+
     system("cls");
     initBoard();
-    while (1){
+    while (1) {
         boardDelBlock();
         if (kbhit()){
             char c = getch();
-            if (c=='a' && canMove(-1,0)) x--;
-            if (c=='d' && canMove(1,0) ) x++;
-            if (c=='x' && canMove(0,1))  y++;
-            if (c=='w') rotateBlock();
-            if (c=='q') break;
+            if (c == 'a' && canMove(-1, 0)) x--;
+            if (c == 'd' && canMove(1, 0)) x++;
+            if (c == 'x' && canMove(0, 1)) y++;
+            if (c == 'w') currentPiece->rotate(x, y);
+            if (c == 'q') break;
         }
-        if (canMove(0,1)) y++;
+        if (canMove(0, 1)) y++;
         else {
             block2Board();
             removeLine();
-            x = 5; y = 0; b = rand() % 7;
+            x = 5; y = 0;
+            delete currentPiece; 
+            currentPiece = createRandomPiece();
         }
         block2Board();
         draw();
